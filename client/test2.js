bafbf58772933444cccbe76dc578838fa823fcfe
Miguel M <113144211+msftmikey@users.noreply.github.com>
2023-04-08 12:21:04 -0700

    Added Comments

--- client/test2.js
+++ client/test2.js
@@ -1,5 +1,5 @@
 //This code below is a multiplication table from a website to prove code works in this program. THIS IS NOT OUR CODE. 
-//Credit: https://www.guru99.com/practical-code-examples-using-javascript.html
+//Credit: https://www.guru99.com/practical-code-examples-using-javascript.html 
 
 <html>
 <head>
